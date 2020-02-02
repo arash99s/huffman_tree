83870e51c7a55065c7daa3f8f22fa9d886217056
arash <arash99s@yahoo.com>
2020-02-02 10:30:19 +0330

    add relative address of start.sh

--- WIN/mainwindow.h
+++ WIN/mainwindow.h
@@ -17,7 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QString start = "/home/arash/github_repositories/huffman_tree/start.sh";
+    QString start = "../start.sh";
 
 private slots:
     void on_pushButton_clicked();
